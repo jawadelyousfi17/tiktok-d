87115f78b19d7db6090d1acc8cbfbbecb86cf01d
Jawad el yousfi <Jawad el yousfi>
2024-06-22 14:49:26 +0100

    add image card

--- app/not-found.js
+++ app/not-found.js
@@ -14,7 +14,7 @@ export default function Custom404() {
   };
 
     return <section className=" w-full md:w-1/2 mx-auto  pb-20  flex flex-col items-center ">
-       <Lottie options={defaultOptions} height={ } width={ } />   
+       <Lottie options={defaultOptions} height={400} width={400} />   
           <h2 className='text-3xl font-bold'>Could not find this page</h2>
 
     </section>
